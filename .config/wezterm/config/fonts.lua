f2f961c0769ea92c10e8554f893b43a3f5b26c21
nfwyst <nfwyst@gmail.com>
2024-12-13 22:45:58 +0800

    chore: misc

--- .config/wezterm/config/fonts.lua
+++ .config/wezterm/config/fonts.lua
@@ -2,14 +2,15 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 local font = '0xProto Nerd Font'
-local font_size = platform().is_mac and 18 or 16
+local font_size = platform().is_mac and 16 or 14
 
 return {
   font = wezterm.font(font, {
-    weight = 'ExtraBold',
+    weight = 'Bold',
+    italic = true,
   }),
   font_size = font_size,
-  line_height = 1.15,
+  line_height = 1.1,
 
   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
   freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
