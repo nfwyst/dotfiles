c4779828f1d70ead19cc3bf7ef1c01e12015efcb
nfwyst <nfwyst@gmail.com>
2024-12-23 00:33:42 +0800

    chore: misc

--- .config/nvim-lazy/lua/plugins/lsp/mason.lua
+++ .config/nvim-lazy/lua/plugins/lsp/mason.lua
@@ -7,11 +7,28 @@ local ensure_installed = {
   "beautysh",
   "js-debug-adapter",
   "stylua",
+  "gitui",
 }
 
 return {
   "williamboman/mason.nvim",
   opts = {
     ensure_installed = ensure_installed,
+  },
+  keys = {
+    {
+      "<leader>gG",
+      function()
+        Snacks.terminal({ "gitui" })
+      end,
+      desc = "GitUi (cwd)",
+    },
+    {
+      "<leader>gg",
+      function()
+        Snacks.terminal({ "gitui" }, { cwd = LazyVim.root.get() })
+      end,
+      desc = "GitUi (Root Dir)",
+    },
   },
 }
