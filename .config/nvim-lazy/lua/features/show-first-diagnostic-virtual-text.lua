358985500affca9d5db177aadb0700e736fb5b31
nfwyst <nfwyst@gmail.com>
2025-02-09 20:14:53 +0800

    feat: only show diagnostic arround cursor

--- .config/nvim-lazy/lua/features/show-first-diagnostic-virtual-text.lua
+++ .config/nvim-lazy/lua/features/show-first-diagnostic-virtual-text.lua
@@ -1,13 +1,18 @@
 local virtual_text = diagnostic.handlers.virtual_text
 local show = virtual_text.show
 
-local diagnostics_filter = function(diagnostics)
+local diagnostics_filter = function(diagnostics, win)
+  local row = WIN_CURSOR(win)[1]
   local filtered_map = {
     [severity.ERROR] = false,
     [severity.WARN] = false,
     [severity.HINT] = false,
     [severity.INFO] = false,
   }
+
+  table.sort(diagnostics, function(a, b)
+    return math.abs(a.lnum - row) < math.abs(b.lnum - row)
+  end)
 
   return filter(function(diagnostic)
     local st = diagnostic.severity
@@ -24,5 +29,5 @@ local diagnostics_filter = function(diagnostics)
 end
 
 virtual_text.show = function(namespace, bufnr, diagnostics, opts)
-  show(namespace, bufnr, diagnostics_filter(diagnostics), opts)
+  show(namespace, bufnr, diagnostics_filter(diagnostics, fn.bufwinid(bufnr)), opts)
 end
