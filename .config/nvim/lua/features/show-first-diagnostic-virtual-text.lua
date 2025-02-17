fb364dccfbc0cb760a6288118dec5370483c462e
malishengo <143467188+malishengo@users.noreply.github.com>
2025-02-17 14:31:56 +0800

    fix: eslint_d diagnostic

--- .config/nvim/lua/features/show-first-diagnostic-virtual-text.lua
+++ .config/nvim/lua/features/show-first-diagnostic-virtual-text.lua
@@ -1,6 +1,5 @@
 local virtual_text = diagnostic.handlers.virtual_text
 local show = virtual_text.show
-local eslint_fixed = false
 
 local diagnostics_filter = function(diagnostics, win)
   if not api.nvim_win_is_valid(win) then
@@ -22,14 +21,6 @@ local diagnostics_filter = function(diagnostics, win)
   return filter(function(diagnostic)
     local st = diagnostic.severity
     local filtered = filtered_map[st]
-
-    if not eslint_fixed then
-      if STR_CONTAINS(diagnostic.message, "eslintrc") then
-        pcall(cmd.MasonInstall, "eslint_d@13.1.2")
-        eslint_fixed = true
-      end
-    end
-
     if filtered then
       return false
     end
@@ -41,5 +32,43 @@ local diagnostics_filter = function(diagnostics, win)
 end
 
 virtual_text.show = function(namespace, bufnr, diagnostics, opts)
+  ---@diagnostic disable-next-line: need-check-nil
   show(namespace, bufnr, diagnostics_filter(diagnostics, fn.bufwinid(bufnr)), opts)
 end
+
+local diagnostic_cmd
+
+local function is_fe_ft(buf)
+  local filetype = OPT("filetype", { buf = buf })
+  return contains({
+    "javascript",
+    "typescript",
+    "javascriptreact",
+    "typescriptreact",
+  }, filetype)
+end
+
+local eslint_checked = false
+diagnostic_cmd = AUCMD("DiagnosticChanged", {
+  callback = function(event)
+    local diagnostic = event.data.diagnostics[1]
+    if not diagnostic then
+      return
+    end
+
+    if STR_CONTAINS(diagnostic.message, "eslintrc") then
+      pcall(cmd.MasonInstall, "eslint_d@13.1.2")
+      api.nvim_del_autocmd(diagnostic_cmd)
+    elseif is_fe_ft(event.buf) then
+      api.nvim_del_autocmd(diagnostic_cmd)
+    end
+
+    if not eslint_checked then
+      local version = fn.system(ESLINT_BIN_PATH .. " --version")
+      if STR_CONTAINS(version, "13.1.2") then
+        api.nvim_del_autocmd(diagnostic_cmd)
+      end
+      eslint_checked = true
+    end
+  end,
+})
