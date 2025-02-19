d10f3bdf4c8ad29cfcb95c68a1a6b9cdde245dab
nfwyst <nfwyst@gmail.com>
2025-02-20 01:31:56 +0800

    fix: octo

--- .config/nvim/lua/plugins/util/octo.lua
+++ .config/nvim/lua/plugins/util/octo.lua
@@ -1,5 +1,6 @@
 return {
   "pwntester/octo.nvim",
+  dependencies = { "Kaiser-Yang/blink-cmp-git" },
   cond = IS_LAUNCH_FROM_GIT_REPO,
   opts = function(_, opts)
     ADD_BLINK_SOURCE("git", { "octo", "gitcommit", "markdown" }, {
