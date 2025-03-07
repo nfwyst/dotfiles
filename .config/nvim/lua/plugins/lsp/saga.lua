ce64f5b52126dc5dbf0e7a6c3c9274b1aed027bb
nfwyst <nfwyst@gmail.com>
2025-03-08 00:06:23 +0800

    chore: misc

--- .config/nvim/lua/plugins/lsp/saga.lua
+++ .config/nvim/lua/plugins/lsp/saga.lua
@@ -5,6 +5,8 @@ return {
     { "<leader>cL", "", desc = "lsp saga" },
     { "<leader>cLi", "<cmd>Lspsaga incoming_calls<cr>", desc = "Lspsaga: Incoming Calls" },
     { "<leader>cLo", "<cmd>Lspsaga outgoing_calls<cr>", desc = "Lspsaga: Outgoing Calls" },
+    { "<leader>cLt", "<cmd>Lspsaga subtypes<cr>", desc = "Lspsaga: Subtypes" },
+    { "<leader>cLT", "<cmd>Lspsaga supertypes<cr>", desc = "Lspsaga: SuperTypes" },
   },
   opts = {
     implement = {
@@ -14,10 +16,10 @@ return {
       enable = false,
     },
     lightbulb = {
-      enable = not IS_LINUX,
+      enable = false,
     },
     beacon = {
-      enable = not IS_LINUX,
+      enable = false,
     },
   },
 }
