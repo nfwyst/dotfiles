6bd7f0de7e347048b5e0aaedce25d569b26a6dde
nfwyst <nfwyst@gmail.com>
2025-03-09 01:26:12 +0800

    feat: regex pattern viewer

---
+++ .config/nvim/lua/plugins/util/patterns.lua
@@ -0,0 +1,8 @@
+return {
+  "OXY2DEV/patterns.nvim",
+  keys = {
+    { "<leader>cup", "", desc = "regex preview" },
+    { "<leader>cupe", "<cmd>Patterns explain<cr>", desc = "Patterns: Explain under cursor" },
+    { "<leader>cuph", "<cmd>Patterns hover<cr>", desc = "Patterns: Hover under cursor" },
+  },
+}
