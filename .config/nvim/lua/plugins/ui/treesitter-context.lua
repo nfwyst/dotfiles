eaea072e0fd63bc9545eb778c6e491bc93d8ebb9
nfwyst <nfwyst@gmail.com>
2025-03-09 20:29:42 +0800

    feat: goto super scope

--- .config/nvim/lua/plugins/ui/treesitter-context.lua
+++ .config/nvim/lua/plugins/ui/treesitter-context.lua
@@ -1,7 +1,24 @@
+local max_lines
+
+if IS_LINUX then
+  max_lines = 3
+end
+
 return {
   "nvim-treesitter/nvim-treesitter-context",
-  cond = not IS_LINUX,
+  keys = {
+    {
+      "gC",
+      function()
+        require("treesitter-context").go_to_context()
+      end,
+      desc = "Goto Super Scope",
+    },
+  },
   opts = {
+    max_lines = max_lines,
+    zindex = 30,
+    min_window_height = 10,
     on_attach = function(bufnr)
       return not IS_BIG_FILE(bufnr)
     end,
