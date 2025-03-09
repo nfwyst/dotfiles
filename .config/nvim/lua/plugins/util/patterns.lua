676e5f6d6be61b64ab618cb7460a48d0ad356eb8
nfwyst <nfwyst@gmail.com>
2025-03-09 17:22:53 +0800

    chore: typo

--- .config/nvim/lua/plugins/util/patterns.lua
+++ .config/nvim/lua/plugins/util/patterns.lua
@@ -2,7 +2,7 @@ return {
   "OXY2DEV/patterns.nvim",
   keys = {
     { "<leader>cup", "", desc = "regex preview" },
-    { "<leader>cupe", "<cmd>Patterns explain<cr>", desc = "Patterns: Explain under cursor" },
-    { "<leader>cuph", "<cmd>Patterns hover<cr>", desc = "Patterns: Hover under cursor" },
+    { "<leader>cupe", "<cmd>Patterns explain<cr>", desc = "Patterns: Explain Under Cursor" },
+    { "<leader>cuph", "<cmd>Patterns hover<cr>", desc = "Patterns: Hover Under Cursor" },
   },
 }
