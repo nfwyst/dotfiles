ded796433692ef1f332b1c8bd7d4f6922e030f4c
nfwyst <nfwyst@gmail.com>
2025-09-10 15:03:11 +0800

    fix: completion confuse...

--- .config/nvim/lua/plugins/coding/blink.lua
+++ .config/nvim/lua/plugins/coding/blink.lua
@@ -13,6 +13,46 @@ end
 
 local cancel = { "cancel", "fallback" }
 
+local snippet_prefix = ";"
+local emoji_prefix = ":"
+local function should_show(ctx, prefix)
+  -- dont show in command line mode
+  if ctx.mode == "cmdline" then
+    return false
+  end
+
+  -- fix trigger state loss
+  local is_prefix_match = ctx.line:match(prefix .. "%w*$") ~= nil
+  if is_prefix_match then
+    return true
+  end
+
+  local trigger = ctx.trigger
+  -- show when triggered by trigger character
+  return trigger.initial_character == prefix and trigger.initial_kind == "trigger_character"
+end
+
+local function should_show_snippets(ctx)
+  if not should_show(ctx, snippet_prefix) then
+    return false
+  end
+
+  -- show when not in snippets completion mode
+  return not require("blink.cmp").snippet_active()
+end
+
+local function should_show_emoji(ctx)
+  return should_show(ctx, emoji_prefix)
+end
+
+local function should_show_items(ctx)
+  return not should_show_snippets(ctx) and not should_show_emoji(ctx)
+end
+
+local function emoji_trigger()
+  return { emoji_prefix }
+end
+
 return {
   "saghen/blink.cmp",
   event = "CmdlineEnter",
@@ -56,11 +96,11 @@ return {
           emoji = {
             module = "blink-emoji",
             name = "Emoji",
+            min_keyword_length = 1,
+            override = { get_trigger_characters = emoji_trigger },
             opts = {
               insert = true,
-              trigger = function()
-                return { ":" }
-              end,
+              trigger = emoji_trigger,
             },
           },
           dictionary = {
@@ -69,24 +109,53 @@ return {
             name = "Dict",
             max_items = 2,
             min_keyword_length = 3,
+            should_show_items = should_show_items,
             opts = {
               dictionary_directories = { vim.fn.expand("~") .. "/.config/dictionaries" },
             },
           },
           buffer = {
             score_offset = 5,
-            min_keyword_length = 3,
+            min_keyword_length = 2,
+            should_show_items = should_show_items,
           },
           path = {
             score_offset = 25,
-            min_keyword_length = 2,
+            fallbacks = { "snippets", "buffer" },
+            opts = {
+              label_trailing_slash = true,
+              show_hidden_files_by_default = true,
+            },
           },
           lsp = {
             score_offset = 125,
+            should_show_items = should_show_items,
           },
           snippets = {
             score_offset = 625,
             min_keyword_length = 2,
+            should_show_items = should_show_snippets,
+            override = {
+              get_trigger_characters = function()
+                return { snippet_prefix }
+              end,
+            },
+            transform_items = function(ctx, items)
+              local prev_line = ctx.cursor[1] - 1
+              for _, item in ipairs(items) do
+                if not item.trigger_text_modified then
+                  item.trigger_text_modified = true
+                  item.textEdit = {
+                    newText = item.insertText or item.label,
+                    range = {
+                      start = { line = prev_line, character = ctx.bounds.start_col - 2 },
+                      ["end"] = { line = prev_line, character = ctx.cursor[2] },
+                    },
+                  }
+                end
+              end
+              return items
+            end,
             opts = {
               search_paths = { vim.fn.stdpath("config") .. "/snippets" },
             },
