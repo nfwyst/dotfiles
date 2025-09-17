d468ba07730e9d4b78d72a6e1f71ae616ce6a3f3
nfwyst <nfwyst@gmail.com>
2025-09-17 23:24:10 +0800

    chore: ecolog keymap

--- .config/nvim/lua/plugins/coding/ecolog.lua
+++ .config/nvim/lua/plugins/coding/ecolog.lua
@@ -1,22 +1,45 @@
 return {
   "ph1losof/ecolog.nvim",
   branch = "beta",
+  lazy = false,
+  dependencies = {
+    "folke/which-key.nvim",
+  },
   keys = {
     { "<leader>cEg", "<cmd>EcologGoto<cr>", desc = "Go To Env File" },
-    { "<leader>cEp", "<cmd>EcologPeek<cr>", desc = "Ecolog Peek Variable" },
     { "<leader>cEs", "<cmd>EcologSelect<cr>", desc = "Switch Env File" },
+    { "<leader>cEl", "<Cmd>EcologShelterLinePeek<cr>", desc = "Peek Line" },
+    { "<leader>cEc", "<Cmd>EcologCopy<cr>", desc = "Copy Value Under Cursor" },
+    { "<leader>cEi", "<Cmd>EcologInterpolationToggle<cr>", desc = "Toggle Interpolation" },
+    { "<leader>cEp", "<cmd>EcologSnacks<cr>", desc = "Open A Picker" },
+    { "<leader>cEP", "<cmd>EcologPeek<cr>", desc = "Ecolog Peek Variable" },
+    { "<leader>cEt", "<Cmd>EcologShellToggle<cr>", desc = "Toggle Shell Variables" },
+    { "<leader>cET", "<cmd>EcologShelterToggle<cr>", desc = "Shelter Toggle" },
   },
-  lazy = false,
-  opts = {
-    integrations = {
-      blink_cmp = true,
-    },
-    shelter = {
-      modules = {
-        snacks_previewer = true,
+  config = function()
+    require("which-key").add({
+      { "<leader>cE", "environment variable" },
+    })
+    require("ecolog").setup({
+      preferred_environment = "local",
+      types = true,
+      monorepo = {
+        enabled = true,
+        auto_switch = true,
+      },
+      interpolation = {
+        enabled = true,
+      },
+      integrations = {
+        blink_cmp = true,
         snacks = true,
       },
-    },
-    monorepo = true,
-  },
+      shelter = {
+        modules = {
+          snacks_previewer = true,
+          snacks = true,
+        },
+      },
+    })
+  end,
 }
