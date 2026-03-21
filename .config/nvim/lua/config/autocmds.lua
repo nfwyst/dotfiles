9e5d1b51a70a8982b28bfcab4c4f91939bcbe811
nfwyst <nfwyst@gmail.com>
2026-03-21 10:09:12 +0800

    chore: misc

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -14,3 +14,15 @@ vim.api.nvim_create_autocmd("FileType", {
     end)
   end,
 })
+
+-- Fix E828: Cannot open undo file for writing (filename too long on macOS)
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+  group = vim.api.nvim_create_augroup("undo_file_check", { clear = true }),
+  callback = function(args)
+    local undofile = vim.fn.undofile(vim.api.nvim_buf_get_name(args.buf))
+    -- Filename length limit on macOS is 255
+    if #vim.fn.fnamemodify(undofile, ":t") > 255 then
+      vim.bo[args.buf].undofile = false
+    end
+  end,
+})
