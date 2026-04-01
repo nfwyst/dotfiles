55095fba7bfb5645b107cbc75ac229a82c2dce5f
user <user@gmail.com>
2026-04-01 10:48:13 +0800

    config: disable spellcheck for markdown files
    Keep wrap enabled for markdown but skip spell. Other text filetypes
    (text, plaintex, typst, gitcommit) still get both wrap and spell.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -95,7 +95,9 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
-    vim.opt_local.spell = true
+    if vim.bo.filetype ~= "markdown" then
+      vim.opt_local.spell = true
+    end
   end,
 })
 
